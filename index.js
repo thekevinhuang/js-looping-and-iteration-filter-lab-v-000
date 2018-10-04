ab3f9525b5d8f5f843b95814e4fe49987819d799
Kevin Huang <yellow.khaos@gmail.com>
2018-10-04 21:55:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Code your solution in this file
 function findMatching(collection, name) {
 
-  let result = collection.filter(function(individual) {return name.toUpperCase()===individual.name.toUpperCase()})
+  let result = collection.filter(function(individual) {return name.toUpperCase()===individual.toUpperCase()})
   return result
 }
 
